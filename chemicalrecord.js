3344f2470f9cfb3616c8b0b192957b64908bb109
bdey05 <73259986+bdey05@users.noreply.github.com>
2023-05-26 20:15:44 -0400

    Add files via upload

--- chemicalrecord.js
+++ chemicalrecord.js
@@ -1,11 +1,25 @@
 let nameCount = 2;
 
+window.onload = () => {
+    document.getElementById("initialname").addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+      nameCount--;
+      if (nameCount == 2) {
+        document.getElementById("names").children[0].children[1].disabled = true;
+      }
+    });
+}
+
 let createNameField = () => {
- 
+  if (nameCount == 2) {
+    document.getElementById("names").children[0].children[1].disabled = false;
+  }
+
   let nameField = document.createElement("INPUT");
   nameField.setAttribute("type", "text");
-  nameField.setAttribute("Placeholder", "Name " + nameCount);
+  nameField.setAttribute("Placeholder", "Chemical Name");
   nameField.setAttribute("Name", "Name " + nameCount);
+  nameField.required = true;
 
   let addbtn = document.createElement("button");
   addbtn.setAttribute("type", "button");
@@ -17,40 +31,79 @@ let createNameField = () => {
   let deletebtn = document.createElement("button");
   deletebtn.setAttribute("type", "button");
   deletebtn.setAttribute("value", "Delete");
-  //deletebtn.addEventListener("click", deleteNameField);
   deletebtn.addEventListener("click", (e) => {
-    //console.log(e.target.parentNode)
     e.target.parentNode.remove();
     nameCount--;
+    if (nameCount == 2) {
+      document.getElementById("names").children[0].children[1].disabled = true;
+    }
   });
 
   let btntext = document.createTextNode("Delete");
   deletebtn.append(btntext);
 
-  
-  /*console.log(document.getElementById("names").children[0])
-  if (document.getElementById("names").children[0].children.length == 2)
-  {
-    console.log(document.getElementById("names").children[0]);
-    document.getElementById("names").children[0].append(deletebtn);
-  }*/
-
   let nameDiv = document.createElement("div");
   nameDiv.classList.add("nameDiv");
   nameDiv.append(nameField);
 
-  nameDiv.append(addbtn);
   nameDiv.append(deletebtn);
 
   document.getElementById("names").appendChild(nameDiv);
   nameCount++;
 };
 
-let deleteNameField = () => {
+let generateChemicalJSON = () => {
+  document.getElementById("error").innerHTML = "";
+  document.getElementById("chemicalRecordOutput").children[0].innerHTML = "";
+  document.getElementById("chemicalRecordOutput").children[1].innerHTML = "";
+  const chemicalNames = []
+  for (let i = 0; i < document.getElementById("names").children.length; i++)
+  {
+    chemicalNames.push(document.getElementById("names").children[i].children[0].value);
+  }
+  const CAS = document.getElementById("CASinput").value;
+  if (!CASValidator(CAS))
+  {
+    return displayError("Invalid CAS Number");
+  }
 
-  console.log(nameCount);
+  let chemicalJSON = {
+    "names": chemicalNames,
+    "CAS": CAS
+  };
+  document.getElementById("chemicalRecordOutput").style.display = "flex";
+  document.getElementById("copy").style.display = "block";
+  document.getElementById("chemicalRecordOutput").children[0].innerHTML = "Generated Chemical Record: ";
+  document.getElementById("chemicalRecordOutput").children[1].innerHTML = JSON.stringify(chemicalJSON, null, 4);
+}
 
-  let names = document.getElementById("names");
-  names.removeChild(names.lastChild);
-  nameCount--;
+let CASValidator = (CAS) => {
+  if (!CAS || !CAS.match(/[0-9]{2,7}-[0-9]{2}-[0-9]/)) {
+    return false;
+  }
+
+  let sum = 0;
+  let digits = CAS.replace(/-/g, '');
+
+  for (let i = digits.length - 2; i >= 0; i--) {
+    sum += parseInt(digits[i]) * (digits.length - i - 1);
+  }
+  
+  return sum % 10 === parseInt(CAS.slice(-1));
+}
+
+let displayError = (error) => {
+  document.getElementById("chemicalRecordOutput").style.display = "flex";
+  document.getElementById("copy").style.display = "none";
+
+  
+  document.getElementById("error").innerHTML = error;
+}
+
+let copyJSON = () => {
+  let copiedJSON = document.getElementById("JSONChemicalRecord");
+  navigator.clipboard.writeText(copiedJSON.innerHTML);
+  document.getElementById("snackbar").className = "show";
+  setTimeout(() => { document.getElementById("snackbar").className = document.getElementById("snackbar").className.replace("show", ""); }, 3000);
+
 }
